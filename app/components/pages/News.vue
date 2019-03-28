1f43b93e502e286a7fc602c89d011c3784795be5
Pierre-Yves Gosset <pyg@framasoft.org>
2019-03-28 16:20:24 +0100

    Revert "YOLO!"
    This reverts commit add4bb595a79563a8e251cd37288206499a7455e

--- app/components/pages/News.vue
+++ app/components/pages/News.vue
@@ -7,10 +7,9 @@
             <div class="container">
                 <h1>{{ $t('news.title') }}</h1>
                 <span>{{ $t('news.subtitle') }}</span>
-                <div v-for="(key,bloc) in $t('news.blocs')">
-                    <a :href="`#${key}`"><h2 :id="key">{{ bloc.title }}</h2></a>
+                <div v-for="bloc in $t('news.blocs')">
+                    <a :href="`#${bloc.id}`"><h2 :id="bloc.id">{{ bloc.title }}</h2></a>
                     <p v-for="p in bloc.text" v-html="p"></p>
-                    <hr />
                 </div>
             </div>
         </section>
