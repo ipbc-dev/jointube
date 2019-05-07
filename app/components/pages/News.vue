76ae9b1fc209f55acd2f42bb92852c4273abfe98
Thomas Citharel <tcit@tcit.fr>
2019-05-07 11:03:17 +0200

    Add an aside « latest articles » section
    Remove day of week, add a calendar icon and fix styles
    
    (thx @josephk/JoinMobilizon for the inspiration)
    
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- app/components/pages/News.vue
+++ app/components/pages/News.vue
@@ -3,18 +3,38 @@
         <vue-headful
                 :title="$t('meta.title') + ' - ' + $t('hof.title')"
         />
-        <section class="clearfix faq">
-            <div class="container">
+        <div class="container">
+            <section class="col-lg-8 faq">
                 <h1>{{ $t('news.title') }}</h1>
                 <span>{{ $t('news.subtitle') }}</span>
                 <div v-for="(bloc, key) in $t('news.blocs')" :id="key">
                     <a :href="`#${key}`"><h2>{{ bloc.title }}</h2></a>
-                    <time v-if="bloc.date" :datetime="bloc.date.data">{{ bloc.date.text }}</time>
+                    <p class="small">
+                        <i aria-hidden="true" class="fa fa-calendar"></i>
+                        <time v-if="bloc.date" :datetime="bloc.date.data">{{ bloc.date.text }}</time>
+                    </p>
                     <p v-for="p in bloc.text" v-html="p"></p>
                     <hr />
                 </div>
-            </div>
-        </section>
+            </section>
+            <aside class="col-lg-4">
+                <div class="sticky-top">
+                    <div class="bloc">
+                        <nav>
+                            <h3>{{ $t('news.latest-articles') }}</h3>
+                            <ul>
+                                <li v-for="(bloc, key) in $t('news.blocs')">
+                                    <a :href="`#${key}`">{{ bloc.title }}</a>
+                                    <time v-if="bloc.date" :datetime="bloc.date.data" class="text-muted small">
+                                        {{ bloc.date.text }}
+                                    </time>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </aside>
+        </div>
     </main>
 </template>
 
