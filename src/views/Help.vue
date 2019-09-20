49b2785b9b4827d3c2f846644779fb40d2b024bb
Chocobozzz <me@florianbigard.com>
2019-09-20 11:51:09 +0200

    Set help image sizes

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -12,7 +12,7 @@
 
       <div class="block-with-image understand">
         <div class="image-block">
-          <img :src="buildImgUrl('help/rabbit.png')" alt="Rabbit image">
+          <img width="167px" height="252px" :src="buildImgUrl('help/rabbit.png')" alt="Rabbit image">
         </div>
 
         <div class="text-block">
@@ -49,13 +49,13 @@
         </div>
 
         <div class="image-block">
-          <img :src="buildImgUrl('help/cat.png')" alt="Cat image">
+          <img width="189px" height="205px" :src="buildImgUrl('help/cat.png')" alt="Cat image">
         </div>
       </div>
 
       <div class="block-with-image administrators">
         <div class="image-block">
-          <img :src="buildImgUrl('help/panda.png')" alt="Panda image">
+          <img width="158px" height="219px" :src="buildImgUrl('help/panda.png')" alt="Panda image">
         </div>
 
         <div class="text-block">
@@ -97,7 +97,7 @@
         </div>
 
         <div class="image-block">
-          <img :src="buildImgUrl('help/dog.png')" alt="Dog image">
+          <img width="242px" height="221px" :src="buildImgUrl('help/dog.png')" alt="Dog image">
         </div>
       </div>
 
