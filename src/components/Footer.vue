9273b12dd2c6a880c2536bf3ed0c0ffc0655d8ad
Chocobozzz <me@florianbigard.com>
2019-11-18 09:22:40 +0100

    Add credits

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,33 @@
 <template>
   <footer id="main-footer">
-    <div>
-      <a target="_blank" rel="noopener noreferrer" href="https://framasoft.org/en/legals/" v-translate>Legal notices</a>
 
-      <a target="_blank" rel="noopener noreferrer" href="https://contact.framasoft.org" v-translate>Contact</a>
+    <div class="links">
+      <div>
+        <a target="_blank" rel="noopener noreferrer" href="https://framasoft.org/en/legals/" v-translate>Legal notices</a>
 
-      <a target="_blank" rel="noopener noreferrer" href="https://framalistes.org/sympa/subscribe/peertube-newsletter" v-translate>Newsletter</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://contact.framasoft.org" v-translate>Contact</a>
 
-      <a target="_blank" rel="noopener noreferrer" href="https://framacolibri.org/c/peertube" v-translate>Forum</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://framalistes.org/sympa/subscribe/peertube-newsletter" v-translate>Newsletter</a>
+
+        <a target="_blank" rel="noopener noreferrer" href="https://framacolibri.org/c/peertube" v-translate>Forum</a>
+      </div>
+
+      <div>
+        <a target="_blank" rel="noopener noreferrer" href="https://framagit.org/framasoft/peertube/joinpeertube" v-translate>JoinPeerTube Git</a>
+
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Chocobozzz/PeerTube" v-translate>PeerTube Git</a>
+      </div>
     </div>
 
-    <div>
-      <a target="_blank" rel="noopener noreferrer" href="https://framagit.org/framasoft/peertube/joinpeertube" v-translate>JoinPeerTube Git</a>
+    <div class="credits">
+      <div v-translate>
+        Website developed by <a target="_blank" rel="noopener noreferrer" href="https://framasoft.org">Framasoft</a> and designed by <a target="_blank" rel="noopener noreferrer" href="https://www.maiwann.net/">Maiwann</a>
+      </div>
 
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/Chocobozzz/PeerTube" v-translate>PeerTube Git</a>
+      <div v-translate>
+        Illustrations from <a target="_blank" rel="noopener noreferrer" href="https://framatube.org/videos/watch/9c9de5e8-0a1e-484a-b099-e80766180a6d">What is PeerTube video</a>,
+        created by <a target="_blank" rel="noopener noreferrer" href="https://libreart.info/">LILA</a> - <a target="_blank" rel="noopener noreferrer" href="https://film.zemarmot.net/">ZeMarmot Team</a>
+      </div>
     </div>
   </footer>
 </template>
@@ -26,16 +40,25 @@
     margin-top: 30px;
     background-color: #fff;
     padding: 30px 200px;
-    display: flex;
-    justify-content: space-between;
 
-    > div {
-      a {
-        display: block;
-        color: #000;
-        font-weight: $font-semibold;
-        margin-bottom: 10px;
+    .links {
+      display: flex;
+      justify-content: space-between;
+
+      > div {
+        a {
+          display: block;
+          color: #000;
+          font-weight: $font-semibold;
+          margin-bottom: 10px;
+        }
       }
+    }
+
+    .credits {
+      margin-top: 40px;
+      font-size: 12px;
+      text-align: center;
     }
 
     @media screen and (max-width: $responsive-screen) {
