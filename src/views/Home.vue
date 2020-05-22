8fd0e60d4120e36a4071991a31deae8e63dd4e83
Chocobozzz <me@florianbigard.com>
2020-05-22 10:27:54 +0200

    Remove package json

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,7 +42,7 @@
               </p>
               <p class="card-text text-right">
                 <router-link to="roadmap" class="bottom-link">
-                  More informations
+                  <translate>More informations</translate>
                 </router-link>
               </p>
             </div>
